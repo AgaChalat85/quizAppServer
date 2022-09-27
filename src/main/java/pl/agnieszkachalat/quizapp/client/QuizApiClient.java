977c426b3d4be1b1296c396b86eb49692ea63f99
48530 <48530@LAPTOP-NSH6U059>
2022-09-27 21:49:36 +0200

    getRandomQuestion method returns list of QuestionResponse objects

--- src/main/java/pl/agnieszkachalat/quizapp/client/QuizApiClient.java
+++ src/main/java/pl/agnieszkachalat/quizapp/client/QuizApiClient.java
@@ -1,13 +1,17 @@
 package pl.agnieszkachalat.quizapp.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pl.agnieszkachalat.quizapp.client.dto.QuestionResponseDto;
 
 @Component
 public class QuizApiClient {
@@ -15,11 +19,11 @@ public class QuizApiClient {
     @Autowired private HttpClient httpClient;
     @Autowired private HttpRequest randomQuestionRequest;
     
-    public String getRandomQuestion() {
+    public List<QuestionResponseDto> getRandomQuestion() {
         
         try {
             HttpResponse<String> response = httpClient.send(randomQuestionRequest, HttpResponse.BodyHandlers.ofString());
-            return response.body();
+            return new ObjectMapper().readValue(response.body(), new TypeReference<List<QuestionResponseDto>>() {});
         } catch (IOException | InterruptedException ex) {
             Logger.getLogger(QuizApiClient.class.getName()).log(Level.SEVERE, null, ex);
         }
