2c071e82360a602662a3ffa7fdff30f68d260751
48530 <48530@LAPTOP-NSH6U059>
2022-10-03 21:26:23 +0200

    QuizApiWebserviceTest with tests created

--- src/main/java/pl/agnieszkachalat/quizapp/client/QuizApiClient.java
+++ src/main/java/pl/agnieszkachalat/quizapp/client/QuizApiClient.java
@@ -8,7 +8,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/test/java/pl/agnieszkachalat/quizapp/webservice/QuizApiWebserviceTest.java
@@ -0,0 +1,66 @@
+package pl.agnieszkachalat.quizapp.webservice;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import java.net.http.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.agnieszkachalat.quizapp.BaseTest;
+
+@AutoConfigureMockMvc
+public class QuizApiWebserviceTest extends BaseTest {
+    
+    private static final String GET_RANDOM_QUESTION_URI = "/v1/quizApi/randomQuestion";
+    
+    @MockBean private HttpClient httpClient;
+    @MockBean private HttpRequest randomQuestionRequest;
+    @Mock private HttpResponse response;
+    
+    @Autowired private MockMvc mockMvc;
+    
+    @Test
+    public void thatGetRandomQuestionEndpointWorks() throws Exception {
+        String jsonResponseString = getFileAsString("json/QuizApiClientTest_thatGetRandomQuestionWorks.json");
+        
+        Mockito.when(httpClient.send(randomQuestionRequest, HttpResponse.BodyHandlers.ofString())).thenReturn(response);
+        Mockito.when(response.statusCode()).thenReturn(200);
+        Mockito.when(response.body()).thenReturn(jsonResponseString);
+        
+        mockMvc.perform(MockMvcRequestBuilders.get(GET_RANDOM_QUESTION_URI)
+                                              .accept(MediaType.APPLICATION_JSON))
+                                              .andDo(print())
+                                              .andExpect(status().isOk())
+                                              .andExpect(jsonPath("$.qstId").value(1L))
+                                              .andExpect(jsonPath("$.question").value("How to delete a directory in Linux?"));
+    }
+    
+    @Test
+    public void thatGetRandomQuestionEndpointReturnsNotFound() throws Exception {
+        Mockito.when(httpClient.send(randomQuestionRequest, HttpResponse.BodyHandlers.ofString())).thenReturn(response);
+        Mockito.when(response.statusCode()).thenReturn(200);
+        Mockito.when(response.body()).thenReturn("[]");
+        
+        mockMvc.perform(MockMvcRequestBuilders.get(GET_RANDOM_QUESTION_URI)
+                                              .accept(MediaType.APPLICATION_JSON))
+                                              .andExpect(status().isNotFound());
+    }
+    
+    @Test
+    public void thatGetRandomQuestionEndpointReturnsBadRequest() throws Exception {
+        Mockito.when(httpClient.send(randomQuestionRequest, HttpResponse.BodyHandlers.ofString())).thenReturn(response);
+        Mockito.when(response.statusCode()).thenReturn(400);
+        
+        mockMvc.perform(MockMvcRequestBuilders.get(GET_RANDOM_QUESTION_URI)
+                                              .accept(MediaType.APPLICATION_JSON))
+                                              .andExpect(status().isBadRequest());
+    }
+}
