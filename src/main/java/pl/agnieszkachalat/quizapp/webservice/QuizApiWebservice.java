9f74b534386a9d0f5d72424178fb61b2a81afd52
48530 <48530@LAPTOP-NSH6U059>
2022-10-05 20:47:25 +0200

    CrossOrigin annotated

--- src/main/java/pl/agnieszkachalat/quizapp/webservice/QuizApiWebservice.java
+++ src/main/java/pl/agnieszkachalat/quizapp/webservice/QuizApiWebservice.java
@@ -3,6 +3,7 @@ package pl.agnieszkachalat.quizapp.webservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import pl.agnieszkachalat.quizapp.dto.QuestionDto;
 import pl.agnieszkachalat.quizapp.service.QuestionService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping(value = "/v1/quizApi")
 public class QuizApiWebservice {
     
